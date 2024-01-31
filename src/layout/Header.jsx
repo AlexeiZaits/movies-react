3d1c6eef424eb6c760a093d8cb1fd896e300d940
Алексей <zaykuh@gmail.com>
2024-01-31 20:44:25 +0500

    feat: add Header,jsx

---
+++ src/layout/Header.jsx
@@ -0,0 +1,13 @@
+function Header(){
+    return <nav>
+            <div className="nav-wrapper deep-purple lighten-2">
+                <a href="#" className="brand-logo">React Movies</a>
+                <ul id="nav-mobile" className="right hide-on-med-and-down">
+                    <li><a href="collapsible.html">JavaScript</a></li>
+                </ul>
+            </div>
+    </nav>
+    
+}
+
+export {Header} 
